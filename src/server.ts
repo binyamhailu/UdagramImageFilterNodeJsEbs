415c1f8c31de79ab4b13a2396d1d4c3aa7c55781
mengestu <binwelbeck@gmail.com>
2022-08-24 13:20:55 +0300

    fixed the types for express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response, Router } from "express";
 import bodyParser from "body-parser";
-const fs = require ('fs');
+const fs = require("fs");
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 (async () => {
   // Init the Express application
   const app = express();
-  const router: Router = Router()
+  const router: Router = Router();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -28,40 +28,41 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage", async (req:express.Request, res:express.Response) => {
-    let {image_url} = req.query;
-    if(!image_url){
-      return res.send().status(404).json({
-        message:"Invalid query Param, Please Try Again!"
-      })
-    }
-      console.log("image_url", image_url)
-    try {
-      
-      const localUrl = await filterImageFromURL(String(image_url))
-      res.status(200).sendFile(localUrl)
-      //after we send the response to the user we will immiedietly delete the picture
-      res.on("finish",()=>{
-        deleteLocalFiles(Array(localUrl))
-      })
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({
-        message: "Image Processing Error please try another Image"
-      })
-    }
-    // console.log(localUrl)
+  app.get(
+    "/filteredimage",
+    async (req: express.Request, res: express.Response) => {
+      let { image_url } = req.query;
+      if (!image_url) {
+        return res.send().status(404).json({
+          message: "Invalid query Param, Please Try Again!",
+        });
+      }
+      console.log("image_url", image_url);
+      try {
+        const localUrl = await filterImageFromURL(String(image_url));
+        res.status(200).sendFile(localUrl);
 
-  });
+        //after we send the response to the user we will immiedietly delete the picture
+
+        res.on("finish", () => {
+          deleteLocalFiles(Array(localUrl));
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: "Image Processing Error please try another Image",
+        });
+      }
+      // console.log(localUrl)
+    }
+  );
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req:express.Request, res:express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
-
- 
 
   // Start the Server
   app.listen(port, () => {
